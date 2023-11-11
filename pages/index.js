5673b71ffce444ac30ab3afc636933934172f3f9
hadikman <hadikman@gmail.com>
2023-11-11 11:14:23 +0330

    convert svg favicon to ico

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.svg" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <IndexPage />
