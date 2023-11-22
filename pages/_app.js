43a07c49cad53e0b0292645cad62aca531e09810
hadikman <hadikman@gmail.com>
2023-11-22 15:00:17 +0330

    add QueryClientProvider to _app

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,7 @@ import createCache from '@emotion/cache'
 import {prefixer} from 'stylis'
 import rtlPlugin from 'stylis-plugin-rtl'
 import CssBaseline from '@mui/material/CssBaseline'
+import {QueryClientProvider, QueryClient} from '@tanstack/react-query'
 import MainLayout from 'components/layout/main.layout'
 
 import 'styles/globals.scss'
@@ -12,15 +13,18 @@ const cacheRtl = createCache({
   key: 'muirtl',
   stylisPlugins: [prefixer, rtlPlugin],
 })
+const queryClient = new QueryClient()
 
 export default function App({Component, pageProps}) {
   return (
     <MuiThemeProvider>
       <CacheProvider value={cacheRtl}>
-        <MainLayout>
-          <CssBaseline />
-          <Component {...pageProps} />
-        </MainLayout>
+        <QueryClientProvider client={queryClient}>
+          <MainLayout>
+            <CssBaseline />
+            <Component {...pageProps} />
+          </MainLayout>
+        </QueryClientProvider>
       </CacheProvider>
     </MuiThemeProvider>
   )
