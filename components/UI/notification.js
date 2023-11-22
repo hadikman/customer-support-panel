6a698b8e5be807cb89d5e17f7eb5c1ff4bc7fe3f
hadikman <hadikman@gmail.com>
2023-11-22 14:57:38 +0330

    add Notification

---
+++ components/UI/notification.js
@@ -0,0 +1,61 @@
+import Snackbar from '@mui/material/Snackbar'
+import Alert from '@mui/material/Alert'
+import IconButton from '@mui/material/IconButton'
+import Icon from 'components/UI/icon'
+import icons from 'library/icons-name'
+
+const {close} = icons
+
+function Notification({
+  open,
+  onClose,
+  isInfo,
+  isError,
+  isSuccess,
+  message,
+  autoHideDuration = 6000,
+}) {
+  function handleOnCloseSnackbar(event, reason) {
+    if (reason === 'clickaway') {
+      return
+    }
+
+    if (onClose) {
+      onClose('')
+    }
+  }
+
+  return (
+    <Snackbar
+      open={open}
+      anchorOrigin={{vertical: 'bottom', horizontal: 'right'}}
+      onClose={handleOnCloseSnackbar}
+      autoHideDuration={autoHideDuration}
+      action={
+        <IconButton
+          size="small"
+          color="inherit"
+          onClick={handleOnCloseSnackbar}
+        >
+          <Icon name={close} />
+        </IconButton>
+      }
+    >
+      {isInfo || isError || isSuccess ? (
+        <Alert
+          onClose={handleOnCloseSnackbar}
+          variant="filled"
+          severity={
+            (isInfo && 'info') ||
+            (isError && 'error') ||
+            (isSuccess && 'success')
+          }
+        >
+          {message}
+        </Alert>
+      ) : null}
+    </Snackbar>
+  )
+}
+
+export default Notification
