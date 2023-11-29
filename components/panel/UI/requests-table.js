f5fc9d1e30cc87c3d45beeafb69e07d749a21e93
hadikman <hadikman@gmail.com>
2023-11-29 15:04:23 +0330

    add sort functionality into RequestsTable

--- components/panel/UI/requests-table.js
+++ components/panel/UI/requests-table.js
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import useMutateData from 'hook/useMutateData'
+import {useIsFetching} from '@tanstack/react-query'
 import Link from 'next/link'
 import Grid from '@mui/material/Grid'
 import Box from '@mui/material/Box'
@@ -12,7 +13,9 @@ import TableRow from '@mui/material/TableRow'
 import TableCell from '@mui/material/TableCell'
 import TableFooter from '@mui/material/TableFooter'
 import TablePagination from '@mui/material/TablePagination'
+import TableSortLabel from '@mui/material/TableSortLabel'
 import IconButton from '@mui/material/IconButton'
+import {visuallyHidden} from '@mui/utils'
 import Icon from 'components/UI/icon'
 import StyledHeadCell from 'components/panel/UI/styled-head-cell'
 import StyledCell from 'components/panel/UI/styled-cell'
@@ -32,6 +35,57 @@ const {
   lastPage,
 } = icons
 
+const headCells = [
+  {
+    id: '_id',
+    disablePadding: false,
+    label: 'ردیف',
+  },
+  {
+    id: 'name',
+    disablePadding: true,
+    label: 'نام',
+  },
+  {
+    id: 'registerDate',
+    disablePadding: false,
+    label: 'تاریخ ثبت',
+  },
+  {
+    id: 'serviceState',
+    disablePadding: false,
+    label: 'وضعیت',
+  },
+]
+
+function descendingComparator(a, b, orderBy) {
+  if (b[orderBy] < a[orderBy]) {
+    return -1
+  }
+  if (b[orderBy] > a[orderBy]) {
+    return 1
+  }
+  return 0
+}
+
+function getComparator(order, orderBy) {
+  return order === 'desc'
+    ? (a, b) => descendingComparator(a, b, orderBy)
+    : (a, b) => -descendingComparator(a, b, orderBy)
+}
+
+function stableSort(array, comparator) {
+  const stabilizedThis = array.map((el, index) => [el, index])
+  stabilizedThis.sort((a, b) => {
+    const order = comparator(a[0], b[0])
+    if (order !== 0) {
+      return order
+    }
+    return a[1] - b[1]
+  })
+  return stabilizedThis.map(el => el[0])
+}
+
 export default function ReqeustsTable({data, isLoading, isSuccess}) {
   const {
     mutate: mutateToDeleteRequest,
@@ -48,6 +102,8 @@ export default function ReqeustsTable({data, isLoading, isSuccess}) {
     })
 
   const [page, setPage] = React.useState(0)
+  const [order, setOrder] = React.useState('asc')
+  const [orderBy, setOrderBy] = React.useState('')
   const [rowsPerPage, setRowsPerPage] = React.useState(DISPLAY_ROWS)
   const [status, setStatus] = React.useState('')
   let emptyRowsInPage
@@ -65,6 +121,8 @@ export default function ReqeustsTable({data, isLoading, isSuccess}) {
 
   if (isSuccess) {
     const dataLength = data.length
+    data = data.map((d, idx) => ({...d, id: idx + 1}))
+
     emptyRowsInPage =
       page > 0
         ? Math.max(0, (page + 1) * rowsPerPage - dataLength)
@@ -79,6 +137,22 @@ export default function ReqeustsTable({data, isLoading, isSuccess}) {
       setStatus('deleted')
     }
   }, [isPendingDeletion, isDeletedSuccessfully, isUpdatedSuccessfully])
+
+  function handleOnRequestSort(event, property) {
+    const isAsc = orderBy === property && order === 'asc'
+
+    setOrder(isAsc ? 'desc' : 'asc')
+    setOrderBy(property)
+  }
+
+  const visibleRows = React.useMemo(
+    () =>
+      stableSort(data || [], getComparator(order, orderBy)).slice(
+        page * rowsPerPage,
+        page * rowsPerPage + rowsPerPage,
+      ),
+    [data, order, orderBy, page, rowsPerPage],
+  )
 
   function handleOnDeleteRequest(id, name) {
     const isConfirmed = confirm(`آیا مایل به حذف درخواست ${name} هستید؟`)
@@ -105,22 +179,11 @@ export default function ReqeustsTable({data, isLoading, isSuccess}) {
     <>
       <TableContainer component={Paper} sx={{width: '100%'}}>
         <Table>
-          <TableHead>
-            <TableRow>
-              <StyledHeadCell align="center" sx={{px: 1}}>
-                ردیف
-              </StyledHeadCell>
-              <StyledHeadCell align="center" sx={{px: 1}}>
-                نام
-              </StyledHeadCell>
-              <StyledHeadCell align="center" sx={{px: 1}}>
-                تاریخ ثبت
-              </StyledHeadCell>
-              <StyledHeadCell align="center" sx={{px: 1}}>
-                وضعیت
-              </StyledHeadCell>
-            </TableRow>
-          </TableHead>
+          <SortableTableHead
+            order={order}
+            orderBy={orderBy}
+            onRequestSort={handleOnRequestSort}
+          />
           {isLoading ? (
             <TableBody>
               <TableRow sx={{height: ROW_HEIGHT * rowsPerPage}}>
@@ -132,91 +195,93 @@ export default function ReqeustsTable({data, isLoading, isSuccess}) {
           ) : isSuccess ? (
             <>
               <TableBody>
-                {(rowsPerPage > 0
-                  ? data.slice(
-                      page * rowsPerPage,
-                      page * rowsPerPage + rowsPerPage,
-                    )
-                  : data
-                ).map(({_id, name, registerDate, serviceState, seen}, idx) => (
-                  <TableRow
-                    key={_id}
-                    sx={{
-                      '&:nth-of-type(odd)': {
-                        backgroundColor: theme => theme.palette.action.hover,
-                      },
-                      '&:last-child td, &:last-child th': {
-                        border: 0,
-                      },
-                      '.link': {
-                        position: 'absolute',
-                        inset: 0,
-                        width: '100%',
-                        height: '100%',
-                      },
-                    }}
-                  >
-                    <StyledCell align="center">
-                      {DISPLAY_ROWS * page + idx + 1}
-                    </StyledCell>
-                    <StyledCell
-                      sx={{px: 0, cursor: 'pointer', position: 'relative'}}
-                      onClick={() => handleOnClickCell(_id)}
+                {visibleRows.map(
+                  ({_id, id, name, registerDate, serviceState, seen}) => (
+                    <TableRow
+                      key={id}
+                      sx={{
+                        '&:nth-of-type(odd)': {
+                          backgroundColor: theme => theme.palette.action.hover,
+                        },
+                        '&:last-child td, &:last-child th': {
+                          border: 0,
+                        },
+                        '.link': {
+                          position: 'absolute',
+                          inset: 0,
+                          width: '100%',
+                          height: '100%',
+                        },
+                      }}
                     >
-                      {!seen && (
-                        <Box
-                          component="span"
-                          sx={{
-                            position: 'absolute',
-                            top: -6,
-                            left: -6,
-                            width: 6,
-                            height: 6,
-                            border: '6px solid transparent',
-                            borderBottomColor: 'error.main',
-                            transform: 'rotate(45deg)',
-                          }}
-                        ></Box>
-                      )}
-                      <Link
-                        href={`/dash-cp/request?q=${_id}`}
-                        className="link"
-                      ></Link>
-                      {name}
-                    </StyledCell>
-                    <StyledCell align="center">
-                      {registerDate.split('-')[1].slice(6)}
-                    </StyledCell>
-                    <StyledCell>
-                      <Grid
-                        container
-                        sx={{
-                          alignItems: 'center',
-                          justifyContent: 'space-evenly',
-                          gap: 0.5,
-                        }}
+                      <StyledCell align="left">{id}</StyledCell>
+                      <StyledCell
+                        sx={{px: 0, cursor: 'pointer', position: 'relative'}}
+                        onClick={() => handleOnClickCell(_id)}
                       >
-                        {serviceState === 'در حال رسیدگی' ? (
-                          <Icon name={progress} color="warning" size="small" />
-                        ) : serviceState === 'انجام شده' ? (
-                          <Icon
-                            name={completion}
-                            color="success"
-                            size="small"
-                          />
-                        ) : (
-                          <Icon name={progress} color="grey-4" size="small" />
+                        {!seen && (
+                          <Box
+                            component="span"
+                            sx={{
+                              position: 'absolute',
+                              top: -5,
+                              left: -6,
+                              width: 6,
+                              height: 6,
+                              border: '6px solid transparent',
+                              borderBottomColor: 'error.main',
+                              transform: 'rotate(45deg)',
+                            }}
+                          ></Box>
                         )}
-                        <IconButton
-                          size="small"
-                          onClick={() => handleOnDeleteRequest(_id, name)}
+                        <Link
+                          href={`/dash-cp/request?q=${_id}`}
+                          className="link"
+                        ></Link>
+                        {name}
+                      </StyledCell>
+                      <StyledCell align="left">
+                        {new Date(registerDate).toLocaleDateString('fa-IR', {
+                          year: '2-digit',
+                          month: '2-digit',
+                          day: '2-digit',
+                        })}
+                      </StyledCell>
+                      <StyledCell>
+                        <Grid
+                          container
+                          sx={{
+                            alignItems: 'center',
+                            justifyContent: 'flex-start',
+                            gap: 0.5,
+                          }}
                         >
-                          <Icon name={erase} color="error" size="small" />
-                        </IconButton>
-                      </Grid>
-                    </StyledCell>
-                  </TableRow>
-                ))}
+                          {serviceState === 'در حال رسیدگی' ? (
+                            <Icon
+                              name={progress}
+                              color="warning"
+                              size="small"
+                            />
+                          ) : serviceState === 'انجام شده' ? (
+                            <Icon
+                              name={completion}
+                              color="success"
+                              size="small"
+                            />
+                          ) : (
+                            <Icon name={progress} color="grey-4" size="small" />
+                          )}
+                          <IconButton
+                            size="small"
+                            onClick={() => handleOnDeleteRequest(_id, name)}
+                          >
+                            <Icon name={erase} color="error" size="small" />
+                          </IconButton>
+                        </Grid>
+                      </StyledCell>
+                    </TableRow>
+                  ),
+                )}
                 {emptyRowsInPage > 0 && (
                   <TableRow sx={{height: ROW_HEIGHT * emptyRowsInPage}}>
                     <StyledCell colSpan={4} />
@@ -292,6 +357,77 @@ export default function ReqeustsTable({data, isLoading, isSuccess}) {
         isSuccess={isSuccessfulNotification}
       />
     </>
+  )
+}
+
+function SortableTableHead({order, orderBy, onRequestSort}) {
+  const isFetchingAllForms = useIsFetching({queryKey: ['all-forms']})
+
+  const isLoadingAllForms = Boolean(isFetchingAllForms)
+
+  function handleOnCreateSort(property) {
+    return event => onRequestSort(event, property)
+  }
+
+  return (
+    <TableHead
+      sx={{
+        '.sort-root-style': {
+          color: theme => theme.palette.common.white,
+          ':hover': {
+            color: theme => theme.palette.grey[500],
+          },
+        },
+        '& .active.active': {
+          color: theme => theme.palette.common.white,
+        },
+        '.direction-icon': {
+          fill: theme => theme.palette.common.white,
+        },
+      }}
+    >
+      <TableRow
+        sx={{
+          bgcolor: theme => theme.palette.grey[400],
+          borderBottom: isLoadingAllForms ? '6px solid' : '3px solid',
+          borderColor: isLoadingAllForms ? 'warning.light' : 'success.light',
+          transition: 'border 0.5s',
+        }}
+      >
+        {headCells.map(({id, disablePadding, label}) => (
+          <StyledHeadCell
+            key={id}
+            align="left"
+            padding={disablePadding ? 'none' : 'normal'}
+            sortDirection={orderBy === id ? order : false}
+            sx={{
+              ...(id === 'name' && {minWidth: 140}),
+              py: 1,
+              ...(!disablePadding && {px: 1}),
+            }}
+          >
+            <TableSortLabel
+              classes={{
+                root: 'sort-root-style',
+                active: 'active',
+                iconDirectionAsc: 'direction-icon',
+                iconDirectionDesc: 'direction-icon',
+              }}
+              active={orderBy === id}
+              direction={orderBy === id ? order : 'asc'}
+              onClick={handleOnCreateSort(id)}
+            >
+              {label}
+              {orderBy === id ? (
+                <Box component="span" sx={visuallyHidden}>
+                  {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
+                </Box>
+              ) : null}
+            </TableSortLabel>
+          </StyledHeadCell>
+        ))}
+      </TableRow>
+    </TableHead>
   )
 }
 
