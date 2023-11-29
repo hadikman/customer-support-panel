6fc6e855391759a6c3d54fac4cc989fbd36ef7c0
hadikman <hadikman@gmail.com>
2023-11-29 15:01:18 +0330

    update StyledHeadCell with a new component

--- components/panel/UI/styled-head-cell.js
+++ components/panel/UI/styled-head-cell.js
@@ -1,18 +1,11 @@
+import {styled} from '@mui/material/styles'
 import TableCell from '@mui/material/TableCell'
 
-export default function StyledHeadCell({children, align, sx, ...props}) {
-  return (
-    <TableCell
-      align={align}
-      sx={{
-        backgroundColor: theme => theme.palette.common.black,
-        color: theme => theme.palette.common.white,
-        py: 0.5,
-        ...sx,
-      }}
-      {...props}
-    >
-      {children}
-    </TableCell>
-  )
-}
+const StyledHeadCell = styled(TableCell)(({theme}) => ({
+  backgroundColor: theme.palette.common.black,
+  color: theme.palette.common.white,
+  paddingTop: 4,
+  paddingBottom: 4,
+}))
+
+export default StyledHeadCell
